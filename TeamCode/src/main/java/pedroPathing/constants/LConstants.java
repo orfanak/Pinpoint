9482f67790209ae55fcf2e36e511e1b263ec8989
Talos-Edurobots <talos.edurobots@gmail.com>
2025-08-12 13:14:44 +0300

    Switch localization constants to Pinpoint configuration
    Commented out OTOSConstants and added PinpointConstants initialization in LConstants.java. Updated SensorGoBildaPinpointExample to use 'pinpoint' hardware name, new pod offsets, and revised encoder directions for compatibility with Pinpoint odometry.

--- TeamCode/src/main/java/pedroPathing/constants/LConstants.java
+++ TeamCode/src/main/java/pedroPathing/constants/LConstants.java
@@ -9,13 +9,25 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 public class LConstants {
     static {
-        OTOSConstants.useCorrectedOTOSClass = false;
-        OTOSConstants.hardwareMapName = "otos";
-        OTOSConstants.linearUnit = DistanceUnit.INCH;
-        OTOSConstants.angleUnit = AngleUnit.RADIANS;
-        OTOSConstants.offset = new SparkFunOTOS.Pose2D(2.851732283464567, 0, - Math.PI / 2);//Math.PI / 2///
-        OTOSConstants.linearScalar = 0.99345;
-        OTOSConstants.angularScalar = 0.9745;
+//        OTOSConstants.useCorrectedOTOSClass = false;
+//        OTOSConstants.hardwareMapName = "otos";
+//        OTOSConstants.linearUnit = DistanceUnit.INCH;
+//        OTOSConstants.angleUnit = AngleUnit.RADIANS;
+//        OTOSConstants.offset = new SparkFunOTOS.Pose2D(2.851732283464567, 0, - Math.PI / 2);//Math.PI / 2///
+//        OTOSConstants.linearScalar = 0.99345;
+//        OTOSConstants.angularScalar = 0.9745;
+
+        PinpointConstants.forwardY = 0;
+        PinpointConstants.strafeX = 4.9;
+        PinpointConstants.distanceUnit = DistanceUnit.INCH;
+        PinpointConstants.hardwareMapName = "pinpoint";
+        PinpointConstants.useYawScalar = false;
+        PinpointConstants.yawScalar = 1.0;
+        PinpointConstants.useCustomEncoderResolution = false;
+        PinpointConstants.encoderResolution = GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_4_BAR_POD;
+        PinpointConstants.customEncoderResolution = 13.26291192;
+        PinpointConstants.forwardEncoderDirection = GoBildaPinpointDriver.EncoderDirection.REVERSED;
+        PinpointConstants.strafeEncoderDirection = GoBildaPinpointDriver.EncoderDirection.FORWARD;
     }
 }
 
