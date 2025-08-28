48a42239393667a2d4316511194844dc0353799f
orfanak <orfanak@gmail.com>
2025-08-28 12:33:15 +0300

    Update SensorGoBildaPinpointExample.java

--- TeamCode/src/main/java/pedroPathing/tuners_tests/localization/SensorGoBildaPinpointExample.java
+++ TeamCode/src/main/java/pedroPathing/tuners_tests/localization/SensorGoBildaPinpointExample.java
@@ -85,7 +85,8 @@ public class SensorGoBildaPinpointExample extends LinearOpMode {
         the tracking point the Y (strafe) odometry pod is. forward of center is a positive number,
         backwards is a negative number.
          */
-        odo.setOffsets(0, -177.8); //these are tuned for 3110-0002-0001 Product Insight #1
+        // 12.7
+        odo.setOffsets(12.7, -100); //these are tuned for 3110-0002-0001 Product Insight #1
 
         /*
         Set the kind of pods used by your robot. If you're using goBILDA odometry pods, select either
@@ -102,7 +103,7 @@ public class SensorGoBildaPinpointExample extends LinearOpMode {
         increase when you move the robot forward. And the Y (strafe) pod should increase when
         you move the robot to the left.
          */
-        odo.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.REVERSED, GoBildaPinpointDriver.EncoderDirection.FORWARD);
+        odo.setEncoderDirections(GoBildaPinpointDriver.EncoderDirection.REVERSED, GoBildaPinpointDriver.EncoderDirection.REVERSED);
 
 
         /*
